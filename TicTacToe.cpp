fd53f4276a56d1293b69895590aa228a885e6e5b
Amreca <aamreca@umich.edu>
2025-12-09 17:44:57 -0500

    tictactoe

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -3,3 +3,72 @@
 //
 
 #include "TicTacToe.h"
+#include <iostream>
+using namespace std;
+
+TicTacToe::TicTacToe(const Player& p1, const Player& p2)
+    : m_player1(p1), m_player2(p2)
+{
+    m_score = 0;
+}
+void TicTacToe::displayRules() {
+    cout << "***********RULES**********" << endl;
+    cout << "Enter a number (1-9) to place your mark:" << endl;
+    cout << " 1 | 2 | 3 " << endl;
+    cout << " 4 | 5 | 6 " << endl;
+    cout << " 7 | 8 | 9 " << endl;
+    cout << "***************************" << endl;
+}
+int TicTacToe::operator++() {
+    m_score++;
+    return m_score;
+}
+
+void TicTacToe::run() {
+    m_board.clearBoard();
+    displayRules();
+
+    Player current = m_player1;
+    bool gameOver = false;
+    while (!gameOver) {
+        cout << "Board:" << m_board <<  endl;
+
+        int choice;
+        cout << current.getName() << "'s turn (" << current,getSymbol() << ")" << endl;
+        current.getMove(choice);
+
+        if (choice < 1 || choice > 9) {
+            cout << "Invalid choice, Please choose a number between 1-9. " << endl;
+            continue;
+
+        }
+        int row = (choice - 1)/3;
+        int col = (choice - 1)%3;
+        if (!m_board.isValidMove (row, col)) {
+            cout << "Spot Takem. Try Again " << endl;
+            continue;
+        }
+        m_board.makeMove(row, col, current.getSymbol());
+        char winner = m_board.getWinner();
+        if (winner != '') {
+            cout << "\nFinal Board:" << endl;
+            cout << m_board << endl;
+            cout << current.getName() << "wins! (" << winner << ")" << endl;
+            ++(*this);
+            gameOver = true;
+
+        }
+        else if (m_board.isFull()) {
+            cout << "\nFinal Board:" << endl;
+            cout << m_board << endl;
+            cout << "It's A Tie! " << endl;
+            gameOver = true;
+        }
+        else {
+            current = (current.get.Symbol() == m_player1.getSymbol()) ? m_player1 : m_player2;
+
+        }
+    }
+    cout << "\nTotal Score: " << m_score << endl;
+
+}
--- TicTacToe.h
+++ TicTacToe.h
@@ -4,9 +4,26 @@
 
 #ifndef TIC_TAC_TOE_TICTACTOE_H
 #define TIC_TAC_TOE_TICTACTOE_H
+#include "Board.h"
+#include "Game.h"
+#include "Player.h"
 
 
-class TicTacToe {
+class TicTacToe : public Game
+{
+private:
+    Board m_board;
+    Player m_player1;
+    Player m_player2;
+
+public:
+    TicTacToe(const Player& p1, const Player& p2);
+
+    void displayRules() override;
+    void run() override;
+
+    int operator++() override;
+
 };
 
 
